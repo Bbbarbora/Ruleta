438c32ac276fbe2d6892b0e9994c3a9fa027354d
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-02-26 16:03:12 +0100

    final version

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const cislo = Number(prompt('Zadaj cislo od 0 do 36'))
 if (cislo ===0) {
     document.body.innerHTML += `<p> Cislo je 0, nie je ani cierne ani cervene. </p>`;
 } else if (cislo > 36) {
-    document.body.innerHTML += `<p class = "notvalid> Cislo nie je platne. Skus znovu :). </p>`;
+    document.body.innerHTML += `<p class = "notvalid" > Cislo nie je platne. Skus znovu :). </p>`;
 } else if ((cislo >= 1 && cislo <= 10  || cislo >= 19 && cislo <= 28 ) && cislo % 2 === 0 ) {
     document.body.innerHTML += `<p class = "black" > Cislo je parne a cierne. </p>`;
 } else if ((cislo >= 1 && cislo <= 10  || cislo >= 19 && cislo <= 28 ) && cislo % 2 !== 0 ) {
